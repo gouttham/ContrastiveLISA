750b81537e44b26d0148527687e6bbd9fb6607f2
Gouttham <goubala92@gmail.com>
2024-07-30 12:19:06 -0700

    stage2 files

---
+++ runs2.sh
@@ -0,0 +1,13 @@
+deepspeed --master_port=24999 train_stage_2.py \
+  --version=./mbin/test/LLaVA-7B-Lightening-v1-1/ \
+  --constrative --constrative_dataset_dir=/localscratch/gna23/cd-datasets/ \
+  --dataset_dir=/localscratch/gna23/cd-datasets/ \
+  --vision_pretrained=./mbin/sam_vit_h_4b8939.pth \
+  --vision-tower './mbin/clip-vit-large-patch14' \
+  --sample_rates='1' \
+  --epochs='200' \
+  --dataset='contrastive_cd_dataset' \
+  --exp_name="stage2_s2looking" \
+  --batch_size 4 \
+  --steps_per_epoch 1541 \
+  --const_seg_data s2looking
