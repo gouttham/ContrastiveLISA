e089c6c88bfc7fe073e810d230b3e65c6be2544a
Gouttham <goubala92@gmail.com>
2024-08-31 08:15:25 -0700

    stage2 xbd

--- train_stage_v2.py
+++ train_stage_v2.py
@@ -429,7 +429,9 @@ def main(args):
     # pdb.set_trace()
     print("****** Loading stage1 Pretrained weights ******")
     model.load_state_dict(torch.load("./runs/lisa-7b-xbd-14days/ckpt_model/pytorch_model.bin"),strict=False)
-    model.load_state_dict(torch.load("./runs/stage1_xbd/pytorch_model.bin"),strict=True)
+    # model.load_state_dict(torch.load("./runs/stage1_xbd/pytorch_model.bin"),strict=True)
+    model.load_state_dict(torch.load("./runs/stagev1_xbd_fixed_t13/pytorch_model.bin"), strict=True)
+
 
     model_engine, optimizer, _, scheduler = deepspeed.initialize(
         model=model,
