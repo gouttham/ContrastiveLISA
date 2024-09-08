946cfd0f32035432517a611710b9e5073f040202
Gouttham <goubala92@gmail.com>
2024-09-08 11:11:26 -0700

    new pipeline

--- train.py
+++ train.py
@@ -345,7 +345,9 @@ for epoch in range(args.epochs):
             intersection = np.logical_and(pd, gt)
             union = np.logical_or(pd, gt)
             iou_score = np.sum(intersection) / np.sum(union)
-            # if iou_score == None or iou_score == 'NaN':
+            print("iou_score : ",iou_score)
+            if iou_score == None or iou_score == 'NaN':
+                print("Caught")
                 # iou_score = 0
             iou_lists = iou_dict.get(prmpt, [])
             iou_lists.append(iou_score)
