139add9c9a5cdc81b4cff771c74fd6e2db826721
gna23 <gna23@sfu.ca>
2024-10-14 15:00:02 -0700

    debugging

--- debug_loss.py
+++ debug_loss.py
@@ -36,7 +36,7 @@ args = my_utils.parse_args(sys.argv[1:])
 
 # args.exp_name = "NP_S1_cls_1_noCELoss_2"
 # args.exp_name = "NP_S2_cls_1_noCELoss_4"
-args.exp_name = "debugging"
+args.exp_name = "joint_training"
 args.const_seg_data="xbd"
 args.version="./mbin/test/LLaVA-7B-Lightening-v1-1/"
 args.constrative_dataset_dir="/localscratch/gna23/cd-datasets/"
